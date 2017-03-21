7bad88c623af698054709418086db42a6de42cac
Corbyl <cor23@drexel.edu>
2017-03-21 18:47:19 -0400

    Particle Barf
    f

---
+++ MightyBeard/Assets/Script/Boss/RainPukeDamage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainPukeDamage : MonoBehaviour {
+
+    private PlayerStatus ps;
+    public float fallSpeed;
+    public float damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+	}
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.collider.tag.Equals("Player"))
+        {
+            PlayerStatus ps = other.gameObject.GetComponent<PlayerStatus>();
+            ps.decreaseHealth(damage);
+        }
+    }
+}
---
+++ MightyBeard/Assets/particlesy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particlesy : MonoBehaviour {
+
+	public ParticleSystem part;
+	public ParticleSystem part2;
+    public GameObject RainbowPuke;
+    public Transform spawnLoc;
+    public Vector3 posoff;
+    GameObject puke;
+
+    public float pukeLife;
+    public float timeToVomit;
+
+    private bool fireCheck;
+	// Use this for initialization
+	void Start () {
+	
+		part = GetComponent<ParticleSystem> ();
+		part2 = GetComponent<ParticleSystem> ();
+        fireCheck = true;
+        //rb = puke.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeToVomit -= Time.deltaTime;
+
+        if (GetComponent<BossHitState>().health < 50 && (timeToVomit <=0))
+        {
+                puke = (GameObject)Instantiate(RainbowPuke, spawnLoc.position + posoff, spawnLoc.rotation);
+                Debug.Log("F");
+                Destroy(puke, pukeLife);
+                timeToVomit = 10;
+        }
+
+
+    }
+
+    /*void OnParticleCollision (GameObject other){
+
+		Rigidbody2D rigged = other.GetComponent<Rigidbody2D> ();
+
+		if (rigged) {
+			Vector3 direction = other.transform.position - transform.position;
+			direction = direction.normalized;
+			rigged.AddForce(direction * 5);
+			Debug.Log ("I hit you");
+		}
+	}*/
+}
